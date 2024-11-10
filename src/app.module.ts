d09bc52c3e86ada617659f089d58f42b131d6aed
Paul Ehrling <ghostlest330@gmail.com>
2024-11-10 17:34:20 +0100

    I dont know

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/plan_your_day'),
     UserModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+  ) {}
+
+  async create(
+    user: User,
+  ): Promise<{ success: boolean; user?: User; code: number; message: string }> {
+    try {
+      // Check if the user already exists
+      const existingUser = await this.userModel.findOne({ email: user.email });
+      if (existingUser) {
+        return {
+          success: false,
+          code: 409,
+          message: 'User already exists',
+        };
+      }
+
+      const createdUser = new this.userModel(user);
+      await createdUser.save();
+      return {
+        success: true,
+        code: 201,
+        message: 'User created successfully',
+      };
+    } catch (error) {
+      return {
+        success: false,
+        code: 500,
+        message: 'Internal server error',
+      };
+    }
+  }
+}
