2e6e43ae96bd53de3e1998c56bd5188c19502dff
Paul Ehrling <ghostlest330@gmail.com>
2024-11-10 21:44:41 +0100

    Fix import order in app.module.ts for better readability

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
 import { Logger } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
