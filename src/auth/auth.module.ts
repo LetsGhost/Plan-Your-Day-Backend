95b19b117ca329f6a16680cb1ed30632dd6a7944
Paul Ehrling <ghostlest330@gmail.com>
2024-11-10 21:59:53 +0100

    Update JWT expiration time to 5 hours and change secret key in constants

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { AuthController } from './auth.controller';
     PassportModule,
     JwtModule.register({
       secret: 'secretKey',
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '5h' },
     }),
   ],
   controllers: [AuthController],
